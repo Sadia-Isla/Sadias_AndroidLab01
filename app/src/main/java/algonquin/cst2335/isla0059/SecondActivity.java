5858fa67e54bcad9e3a55e867759a8fb7c32b009
Sadia Islam <isla0059@algonquinlive.com>
2021-06-02 18:30:29 -0400

    Week4_3pm

--- app/src/main/java/algonquin/cst2335/isla0059/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/isla0059/SecondActivity.java
@@ -3,7 +3,9 @@ package algonquin.cst2335.isla0059;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -19,5 +21,25 @@ public class SecondActivity extends AppCompatActivity {
         int age = fromPrevious.getIntExtra("Age", 0);
         float notThere = fromPrevious.getFloatExtra("notHere", 4.0f);
         topOfScreen.setText("You typed: " + text);
+
+        Button btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener( clk -> {
+            Intent next = new Intent(Intent.ACTION_DIAL);
+            next.setData(Uri.parse("tel:" + "613 123 4567"));
+            startActivity(next);
+
+        });
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener( click ->
+        {   //one line to return
+            Intent dataBack = new Intent();
+            dataBack.putExtra("Age", 35);
+            dataBack.putExtra("City", "Ottawa");
+
+            setResult(535, dataBack);//send data back to first page
+            finish(); //actuall goes back
+            //onActivityResult() in first page is next
+        });
+
     }
 }
