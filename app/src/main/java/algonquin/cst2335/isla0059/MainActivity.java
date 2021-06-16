1746bd6c85d29715401245c7646dface49ff87d3
Sadia Islam <isla0059@algonquinlive.com>
2021-06-16 15:50:07 -0400

    Week 6

--- app/src/main/java/algonquin/cst2335/isla0059/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/isla0059/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * @author sadia
@@ -17,9 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     /** This holds the text at the centre of the screen*/
     private  TextView tv = null;
-    /** This holds the text at the centre of the screen*/
+    /** This holds the edit text in the middle of the screen*/
     private EditText et = null;
-    /** This holds the text at the centre of the screen*/
+    /** This holds the login button in the screen*/
     private  Button btn = null;
 
     @Override
@@ -27,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
 
          tv = findViewById(R.id.textView);
          et = findViewById(R.id.editText);
@@ -36,6 +38,38 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(clk ->{
             String password = et.getText().toString();
             checkPasswordComplexity(password);
+
+            if(!foundUpperCase)
+            {
+
+                Toast.makeText(getApplicationContext(),"missing an upper case letter",Toast.LENGTH_SHORT).show();  ;// Say that they are missing an upper case letter;
+
+                return true;
+
+            }
+
+            else if( ! foundLowerCase)
+            {
+                Toast.makeText(getApplicationContext(),"missing a lower case letter",Toast.LENGTH_SHORT).show(); ; // Say that they are missing a lower case letter;
+
+                return true;
+
+            }
+
+            else if( ! foundNumber) {
+                Toast.makeText(getApplicationContext()," found a number",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            else if(! foundSpecial) {
+
+                Toast.makeText(getApplicationContext()," found a number",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            else
+
+                return true; //only get here if they're all true
         });
 
     }
@@ -44,24 +78,17 @@ public class MainActivity extends AppCompatActivity {
      * @param pw The String object that we are checking
      * @return Returns true if
      */
-     void checkPasswordComplexity(String pw) {
-         return ;
+     boolean checkPasswordComplexity(String pw) {
+         return pw.contains("ABC");
     }
 
-    /**
-     *
-     * @param c
-     * @return
-     */
-    boolean isSpecialCharacter(char c){
-         switch(c){
-             case '#':
-             case '?':
-             case '*':
-                 return true;
-             default:
-                 return false;
-         }
 
-    }
+
+
+
+
+
+
+
+
 }
