65dab2295f2c09e97f302dc4005190e22b26c800
Sadia Islam <isla0059@algonquinlive.com>
2021-07-21 21:40:49 -0400

    week 10

--- app/src/main/java/algonquin/cst2335/isla0059/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/isla0059/MainActivity.java
@@ -1,6 +1,9 @@
 package algonquin.cst2335.isla0059;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -10,12 +13,17 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -45,32 +53,103 @@ public class MainActivity extends AppCompatActivity {
     String min = null;
     String max = null;
     String humidity = null;
+    TextView tv;
+
+    TextView currentTemp ;
+    TextView maxTemp;
+    TextView minTemp;
+    TextView humidity_tv;
+    TextView description_tv;
+    EditText cityField;
+    ImageView icon ;
+    boolean isHidden = false;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch( item.getItemId() )
+        {
+            case R.id.hide_views:
+                isHidden = !isHidden;
+                currentTemp.setVisibility(View.INVISIBLE);
+                icon.setVisibility(View.INVISIBLE);
+                maxTemp.setVisibility(View.INVISIBLE);
+                minTemp.setVisibility(View.INVISIBLE);
+                humidity_tv.setVisibility(View.INVISIBLE);
+                description_tv.setVisibility(View.INVISIBLE);
+                cityField.setText(" ");
+                break;
+            case R.id.id_increase:
+                float oldSize = currentTemp.getTextSize();
+                float newSize = oldSize + 1;
+                currentTemp.setTextSize(newSize);
+                minTemp.setTextSize(newSize);
+                maxTemp.setTextSize(newSize);
+                humidity_tv.setTextSize(newSize);
+                description_tv.setTextSize(newSize);
+                cityField.setTextSize(newSize);
+                break;
+            case R.id.id_decrease:
+                 oldSize = currentTemp.getTextSize();
+                 newSize = oldSize - 1;
+                currentTemp.setTextSize(newSize);
+                minTemp.setTextSize(newSize);
+                maxTemp.setTextSize(newSize);
+                humidity_tv.setTextSize(newSize);
+                description_tv.setTextSize(newSize);
+                cityField.setTextSize(newSize);
+                break;
+            case 5:
+              String cityName = item.getTitle().toString();
+               runForecast(cityName);
+                break;
 
 
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        icon = findViewById(R.id.icon);
+        tv = findViewById(R.id.temp);
+        tv = findViewById(R.id.minTemp);
+        tv = findViewById(R.id.maxTemp);
+        tv = findViewById(R.id.humidity);
+        tv = findViewById(R.id.description);
+
+       Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
         Button forecastBtn = findViewById(R.id.forecastButton);
         EditText cityText = findViewById(R.id.cityTextField);
 
-        ImageView icon = findViewById(R.id.icon);
-
         forecastBtn.setOnClickListener( click -> {
             String cityName = cityText.getText().toString();
+            myToolbar.getMenu().add( 1, 5, 10, cityName).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+           runForecast(cityName);
             AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
                     .setTitle("Getting forecast")
                     .setMessage("We're calling people in" + cityName + "to look outside their windows and tell us what's the weather look over there.")
                      .setView(new ProgressBar(MainActivity.this))
                      .show();
 
-
             Executor newThread = Executors.newSingleThreadExecutor();
 
             newThread.execute(() -> {
                 try {
-
                     stringURL = "https://api.openweathermap.org/data/2.5/weather?q="
                             + URLEncoder.encode(cityName, "UTF-8")
                             + "&appid=7e943c97096a9784391a981c4d878b22&units=metric&mode=xml";
@@ -96,9 +175,9 @@ public class MainActivity extends AppCompatActivity {
                  case XmlPullParser.START_TAG:
                  if(xpp.getName().equals("temperature"))
                  {
-                  current = xpp.getAttributeValue(null, "value");
-                 min = xpp.getAttributeValue(null, "min");
-                 max = xpp.getAttributeValue(null, "max");
+                     current = xpp.getAttributeValue(null, "value");
+                     min = xpp.getAttributeValue(null, "min");
+                     max = xpp.getAttributeValue(null, "max");
 
                  }
                  else if (xpp.getName().equals("weather"))
@@ -135,30 +214,27 @@ public class MainActivity extends AppCompatActivity {
 
                             image.compress(Bitmap.CompressFormat.PNG, 100, openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
 
-
                         }
                     }
 
-
-
                     runOnUiThread(() -> {
-                        TextView tv = findViewById(R.id.temp);
+
                         tv.setText("The current temperature is" + current);
                         tv.setVisibility(View.VISIBLE);
 
-                        tv = findViewById(R.id.minTemp);
+
                         tv.setText("The min temperature is" + min );
                         tv.setVisibility(View.VISIBLE);
 
-                        tv = findViewById(R.id.maxTemp);
+
                         tv.setText("The max temperature is" + max );
                         tv.setVisibility(View.VISIBLE);
 
-                        tv = findViewById(R.id.humidity);
+
                         tv.setText("The humidity is" + humidity + "%");
                         tv.setVisibility(View.VISIBLE);
 
-                        tv = findViewById(R.id.description);
+
                         tv.setText(description);
                         tv.setVisibility(View.VISIBLE);
 
@@ -179,4 +255,8 @@ public class MainActivity extends AppCompatActivity {
             });
         });//end of lambda function
     }
+
+   private void runForecast(String cityName) {
+    return ;
+   }
 }
